8e4d2f152069df349f7fda86a33515cf815862d1
Rosana Carvalho <carvallhorosana@gmail.com>
2024-10-25 11:03:25 -0300

    parte2 CartaoDeCredito.java

--- CartaoDeCredito.java
+++ CartaoDeCredito.java
@@ -1,22 +1,56 @@
 public class CartaoDeCredito {
-        int numero;
-        String nomeTitular;
-        String cpfTitular;
-        float limite;
-        float totalFatura;;
+        private int numero;
+        private String nomeTitular;
+        private String cpfTitular;
+        private float limite;
+        private float totalFatura;
 
-        public double consultarLimite(){
-            return limite;
+        public int getNumero() {
+                return this.numero;
+        }
+                
+        public void setNumero() {
+                this.numero = numero;
+        }
+    
+        public String getNomeTitular() {
+                return this.nomeTitular;
+        }
+                
+        public void setNomeTitular() {
+                this.nomeTitular = nomeTitular;
         }
 
-        public double consultarTotalFatura(){
-            return totalFatura;
+        public String getCpfTitular() {
+                return this.cpfTitular;
+        }
+                
+        public void  setCpfTitular() {
+                this.cpfTitular = cpfTitular;
+        }
+                
+        public String getLimite() {
+                return this.limite;
+        }
+                
+        public void  setLimite() {
+                this.limite = limite;
+        }
+                
+        public String getTotalFatura() {
+                return this.totalFatura;
+        }
+                
+        public void  setTotalFatura() {
+                this.totalFatura = totalFatura;
         }
 
         public void realizarCompra(double valor) {
             if (valor <= this.limite){
-                this.limite -= valor;
-                this.totalFatura += valor;
+                double limiteAnterior = this.getLimite()
+                this.setLimite(limiteAnterior - valor);
+                double total = this.getTotalFatura();
+                this.setTotalFatura(total + valor);
                 System.out.println("Compra realizada com sucesso!");
             } else {
                 System.out.println("Você não possui limite necessario para essa compra");
