6f44342b1a7efa67dab33c2334594f695a19fd36
Rosana Carvalho <carvallhorosana@gmail.com>
2024-11-01 00:59:28 -0300

    incremento3 Principal.java

--- Principal.java
+++ Principal.java
@@ -2,16 +2,39 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        CartaoDeCredito c = new CartaoDeCredito();
-        c.setCpf("12312352636");
-        c.setLimite(100);
+
+        Scanner entrada = new Scanner(System.in);
         Scanner sc = new Scanner(System.in);
-        int opcao = 1;
+
+        System.out.println("Digite o número do cartão: ");
+        int numero = entrada.nextInt();
+        entrada.nextLine();
+
+        System.out.println("Digite o nome do titular do cartão: ");
+        String nomeTitular = entrada.nextLine();
+
+        System.out.println("Digite o limite de crédito padrão do cartão: ");
+        double limite = entrada.nextDouble();
+        entrada.nextLine();
+
+     
+        CartaoDeCredito c = new CartaoDeCredito();
+        c.setNumero(numero); 
+        c.setNomeTitular(nomeTitular);
+        c.setLimite(limite);
+        
+
+        System.out.println("O número do cartão é: " + numero );
+        System.out.println("O titular do cartão é: " + nomeTitular);
+
+     
+        int opcao = 0;
 
         do {
             System.out.println("1 - Consultar Limite");
             System.out.println("2 - Consultar Fatura");
-            System.out.println("3 - Realizar compra");
+            System.out.println("3 - Realizar compra simples");
+            System.out.println("4 - Realizar compra com cashback");
             System.out.println("0 - Sair");
 
             opcao = sc.nextInt();
@@ -28,6 +51,13 @@ public class Principal {
                     double valor = sc.nextDouble();
                     c.realizarCompra(valor);
                     break;
+                case 4:
+                    System.out.println("Digite o valor da compra");
+                    double valorCompra = sc.nextDouble();
+                    System.out.println("Digite a taxa de cashback: ");
+                    double percentualCashback = entrada.nextDouble();
+                    c.realizarCompra(valorCompra, percentualCashback); 
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
@@ -38,5 +68,6 @@ public class Principal {
         } while (opcao != 0);
 
         sc.close();
+        entrada.close();
     }
 }
