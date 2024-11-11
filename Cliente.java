45ee0f91372ff29bc88b530d5e3852dfd086edbc
Rosana Carvalho <carvallhorosana@gmail.com>
2024-11-10 21:27:15 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,49 @@
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String cnpj;
+    private String email;
+    private String telefone;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return this.cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return this.telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+}
